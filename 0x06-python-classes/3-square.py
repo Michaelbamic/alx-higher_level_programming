1e06faf2f9115f9baea306be651bc9e8b04ae051
Michaelbamic <michaelbamic4shot@gmail.com>
2023-01-25 08:55:08 +0000

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
+
+
 class Square:
-    """ A class that defines a square by its size
-    """
+    """ A class that defines a square by its size"""
+
     def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
+        """ Method to initialize the square object"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -14,6 +14,5 @@ class Square:
             self.__size = size
 
     def area(self):
-        """ Method that returns the square are of the object
-        """
+        """ Method that returns the square are of the object"""
         return (self.__size ** 2)
