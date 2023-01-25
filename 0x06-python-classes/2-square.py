286515054a63f7f3f94538cd1a9440ad5e0d4ad0
Michaelbamic <michaelbamic4shot@gmail.com>
2023-01-25 08:29:11 +0000

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+
 class Square:
     """ A class that defines a square by its size"""
+
     def __init__(self, size=0):
         """ Method to initialize the square object"""
        if not isinstance(size, int):
