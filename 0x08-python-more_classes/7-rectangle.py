9ded39d13c1da6c048f8ccf8939d52ccb47b2500
Michaelbamic <michaelbamic4shot@gmail.com>
2023-01-30 17:31:14 +0000

    correction

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
 
         for i in range(self.__height):
             for j in range(self.__width):
-                string += '#'
+                string += str(self.print_symbol)
             if i < self.__height - 1:
                 string += '\n'
         return string
